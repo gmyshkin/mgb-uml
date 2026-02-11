97480ba3ebbaa45938e76b609663b47a3f062b2c
gmyshkin <gmyshkin@stevens.edu>
2026-02-11 16:56:10 -0500

    Updated linux deployment file to fix name change error

--- deploy-linux.sh
+++ deploy-linux.sh
@@ -8,16 +8,21 @@ chmod +x linuxdeploy*.AppImage
 # 2. Set Environment Variables
 export QMAKE=/usr/bin/qmake6
 
+# --- NEW STEP: Create a temporary MGB-UML desktop file ---
+# We copy the original file and change "Exec=tikzit" to "Exec=mgb-uml"
+cp share/applications/tikzit.desktop mgb-uml.desktop
+sed -i 's/Exec=tikzit/Exec=mgb-uml/g' mgb-uml.desktop
+sed -i 's/Name=TikZiT/Name=MGB-UML/g' mgb-uml.desktop
+
 # 3. Generate the AppImage
-# --- FIX: Changed 'build/tikzit' to 'build/mgb-uml' ---
+# Note: We now use the NEW 'mgb-uml.desktop' file we just created
 ./linuxdeploy-x86_64.AppImage \
     --appdir AppDir \
     --executable build/mgb-uml \
     --icon-file images/tikzit.svg \
-    --desktop-file share/applications/tikzit.desktop \
+    --desktop-file mgb-uml.desktop \
     --plugin qt \
     --output appimage
 
 # 4. Rename for consistency
-# --- FIX: Renaming the output file to mgb-uml ---
 mv MGB-UML*.AppImage mgb-uml-linux.AppImage
